55d1ea69a19cc63da22e0030d43196e76f514794
Zeke Piskulich <piskuliche@ku.edu>
2023-03-08 09:44:50 -0600

    Made a change that allows pickling to be sped

--- wham_derivatives/preprocess_wham.py
+++ wham_derivatives/preprocess_wham.py
@@ -57,6 +57,64 @@ def Write_WHAM_Files(metafile="wham_metadata.info", subfile="final.colvars",subc
             pickle.dump(en,open("wham_pckl/ener_%d.pckl"%window,'wb'))
 
 
+def Write_Single_WHAM_File(window, metafile="wham_metadata.info", subfile="final.colvars",subcol=1,deriv=0, enerfile="flucts.inp" ):
+    """Writes files in a very simple, WHAM-readable format. 
+
+    This takes files, and writes them in a very WHAM-readable format, 
+    mainly by converting them to pckl files in a centralized directory.
+
+    Iargs.deriv sets whether this also reads energies (and writes those to pickle files).
+
+    Args:
+        window (int):   Window number to write
+        metafile (str): Name of metadata file, [default=wham_metadata.info]
+        subfile (str):  Name of subdirectory file, [default=final.colvars]
+        subcol (int):   Column of subfile to read, [default=1]
+        deriv (int):    Whether to read energies, [default=0]
+        enerfile (str): Name of energy file, [default=fluct.inp]
+    
+    Raises:
+        OSError: Input files provided were incorrect, or in the wrong format.
+        OSError: Window number is greater than total number of windows.
+        OSError: Trouble grabbing energies.
+
+    """
+    if not os.path.exists("wham_pckl"):
+        os.makedirs('wham_pckl')
+    
+    nwindows=0
+    try:
+        k=np.genfromtxt(metafile,usecols=1,unpack=True)
+        nwindows = len(k)
+    except:
+        raise OSError("Error: Trouble grabbing windows from metafile")
+    
+    if window > nwindows:
+        raise OSError("Error: Window number %d is greater than total number of windows" % window)
+    
+    etypes = []
+    if deriv == 1:
+        try:
+            etypes=np.genfromtxt(enerfile, usecols=0, dtype=str)
+            print("Grabbing energies")
+            print("Selected energy types:",*etypes)
+        except:
+            raise OSError("Error: Trouble grabbing energies")
+
+    svdata = []
+    # Loops over windows here to write pickle files
+    data, en = [], {}
+    
+    data = np.genfromtxt(str(window)+"/"+subfile,
+                            usecols=subcol,unpack=True)
+                            
+    pickle.dump(data,open('wham_pckl/window_%d.pckl'%window,'wb'))
+
+    if deriv == 1:
+        for key in etypes:
+            en[key]=np.genfromtxt(str(window)+"/"+key+"_init.out",usecols=0)
+        pickle.dump(en,open("wham_pckl/ener_%d.pckl"%window,'wb'))
+
 if __name__ == "__main__":
 
     parser=argparse.ArgumentParser()
